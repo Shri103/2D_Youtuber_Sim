84caabb94a63b6cfe4d58246e3f8cf53de0f4308
Shrihith <shrihith@shrihiths.macbook.air.lan>
2025-12-07 08:47:43 -0500

    New Build with Completion Code 3

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "AppLearn-Youtuber Simulator-1.1";
+const contentToCache = [
+    "Build/2D_Youtuber_Sim.loader.js",
+    "Build/2D_Youtuber_Sim.framework.js",
+    "Build/2D_Youtuber_Sim.data",
+    "Build/2D_Youtuber_Sim.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
